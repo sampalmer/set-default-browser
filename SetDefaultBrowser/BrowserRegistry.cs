b1779b0f133b0525f55d247894f56037fbdde37b
Sam Palmer <sampalmer@users.noreply.github.com>
2017-07-09 11:29:09 +1000

    Fixed issue where browsers are missing "ApplicationName" values in the registry, such as Internet Explorer, weren't recognised.

--- SetDefaultBrowser/BrowserRegistry.cs
+++ SetDefaultBrowser/BrowserRegistry.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,17 +19,20 @@ namespace SetDefaultBrowser
             {
                 var capabilities = TryGetCapabilities(registeredApplication.Value);
                 if (capabilities != null)
-                    if (
-                        capabilities.DisplayName != null
-                        &&
-                        // These protocols are needed for Windows to see it as a web browser.
-                        new[] { "http", "https" }.All(protocol => capabilities.Associations.Contains(protocol, StringComparer.OrdinalIgnoreCase))
-                    )
-                        results.Add(new Browser
-                        {
-                            UniqueApplicationName = registeredApplication.Key,
-                            Capabilities = capabilities,
-                        });
+                {
+                    var associations = capabilities.Associations.Select(association => association.Association).ToArray();
+                    if (new[] { "http", "https" }.All(protocol => associations.Contains(protocol, StringComparer.OrdinalIgnoreCase)))
+                    {
+                        var displayName = TryGetDisplayName(capabilities);
+                        if (displayName != null)
+                            results.Add(new Browser
+                            {
+                                UniqueApplicationName = registeredApplication.Key,
+                                DisplayName = displayName,
+                                Associations = associations,
+                            });
+                    }
+                }
             }
 
             return results.ToArray();
@@ -66,18 +70,83 @@ namespace SetDefaultBrowser
                 {
                     var resourceValue = new StringBuilder(4096);
                     var hresult = WindowsApi.SHLoadIndirectString(rawApplicationName, resourceValue, resourceValue.Capacity, IntPtr.Zero);
-                    result.DisplayName = hresult == 0 ? resourceValue.ToString() : rawApplicationName;
+                    result.ApplicationName = hresult == 0 ? resourceValue.ToString() : rawApplicationName;
                 }
 
-                var associations = new List<string>();
+                var associations = new List<ApplicationAssociation>();
                 foreach (var subkeyName in new[] { "FileAssociations", "URLAssociations" })
                     using (var subkey = key.OpenSubKey(subkeyName))
                         if (subkey != null)
-                            associations.AddRange(subkey.GetValueNames());
+                            foreach (var association in subkey.GetValueNames())
+                            {
+                                var progId = subkey.GetValue(association) as string;
+                                associations.Add(new ApplicationAssociation
+                                {
+                                    Association = association,
+                                    ProgId = progId,
+                                });
+                            }
+
                 result.Associations = associations.ToArray();
             }
 
             return result;
         }
+
+        private static string TryGetDisplayName(ApplicationCapabilities capabilities)
+        {
+            // The display name of a registered application is optional. Internet Explorer doesn't use it, for example.
+            if (capabilities.ApplicationName != null)
+                return capabilities.ApplicationName;
+
+            foreach (var association in capabilities.Associations)
+                if (!String.IsNullOrEmpty(association.ProgId))
+                {
+                    var appName = AssocQueryString(association);
+                    if (appName != null)
+                        return appName;
+                }
+
+            return null;
+        }
+
+        private static string AssocQueryString(ApplicationAssociation association)
+        {
+            uint appNameLength = 0;
+            var result = WindowsApi.AssocQueryString(WindowsApi.AssocF.None, WindowsApi.AssocStr.FriendlyAppName, association.ProgId, null, null, ref appNameLength);
+            if (new[] { 2147943555, 2147942402 }.Contains(result)) // No application is associated with the specified file for this operation OR The system cannot find the file specified
+                return null;
+
+            if (result != 1)
+                throw new Win32Exception((int)result);
+
+            var buffer = new StringBuilder((int)appNameLength);
+            var bufferLength = (uint)buffer.Capacity;
+            result = WindowsApi.AssocQueryString(WindowsApi.AssocF.None, WindowsApi.AssocStr.FriendlyAppName, association.ProgId, null, buffer, ref bufferLength);
+            if (result != 0)
+                throw new Win32Exception((int)result);
+
+            return buffer.ToString();
+        }
+
+        private class ApplicationCapabilities
+        {
+            /// <summary>
+            /// May be <c>null</c>.
+            /// </summary>
+            public string ApplicationName { get; set; }
+            public ApplicationAssociation[] Associations { get; set; }
+        }
+
+        private class ApplicationAssociation
+        {
+            public string Association { get; set; }
+            
+            /// <summary>
+            /// May be null
+            /// </summary>
+            public string ProgId { get; set; }
+        }
+
     }
 }
